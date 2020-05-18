a66669721dd2bbf11ff07c6b99f270f4044637d5
ataykaylar <57875291+ataykaylar@users.noreply.github.com>
2020-05-18 21:09:23 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "AlgebraicExpression.h"
+
+//this is the test code
+int main()
+{
+    cout << infix2prefix("1+2*(2/5)") << endl;
+    cout << infix2prefix("1+2*(2/5)") << endl;
+    cout << evaluatePrefix("+1*2/25") << endl;
+    cout << infix2prefix("1+2*(2/5)") << endl;
+    cout << evaluatePrefix("+1*2/25") << endl;
+    cout << infix2prefix("(4+5)-2*4") << endl;
+    cout << evaluatePrefix("/4+3*-528") << endl;
+    cout << evaluatePrefix("+1*2/25") << endl;
+    cout << infix2prefix("(4+5)-2*4") << endl;
+    cout << evaluatePrefix("/4+3*-528") << endl;
+    cout << infix2prefix("(4+5)-2*4") << endl;
+    cout << infix2prefix("(4+5)-2*4") << endl;
+    cout << evaluatePrefix("/4+3*-528") << endl;
+    cout << evaluatePrefix("+1*2/25") << endl;
+    cout << evaluatePrefix("/4+3*-528") << endl;
+    return 0;
+}
